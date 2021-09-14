b94702644607af5ddec2232f873767b8dfe90bda
Niels de Water <nielsdw@gmail.com>
2021-09-14 16:48:21 +0200

    updates also working, little housekeeping

--- core/views.py
+++ core/views.py
@@ -16,16 +16,7 @@ class AVGRegisterlineViewSet(viewsets.ModelViewSet):
         ext_ref = avg_line.pop("external_reference")
         avg_line = AVGRegisterline.objects.create(**avg_line)
         ExternalReference.objects.create(avgregisterline_id=avg_line.id, **ext_ref)
-"""
-    def perform_update(self, serializer):
-        # print(serializer.data)
-        # print(serializer)
-        serializer.save()
-        #avg_line = serializer.data
-        #ext_ref = avg_line.pop("external_reference")
-        #AVGRegisterline.objects.filter(**avg_line).update(**avg_line)
-        #ExternalReference.objects.filter(**ext_ref).update(**ext_ref)
-"""
+
 
 class ExternalReferenceViewSet(viewsets.ModelViewSet):
     queryset = ExternalReference.objects.all()
